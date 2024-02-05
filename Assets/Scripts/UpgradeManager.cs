e6dab91b29a3e267698a5548d8258bd2de79cce7
Lilli <107088307+LilliC2@users.noreply.github.com>
2024-02-05 15:51:29 +1100

    Upgrade System Resturant

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class UpgradeInformation
+{
+    public string name;
+    public string description;
+    public bool active;
+}
+
 public class UpgradeManager : Singleton<UpgradeManager>
 {
     public GameObject[] recipePosters;
+    public UpgradeInformation[] resturantUpgradeInformation;
+    public UpgradeInformation[] staffUpgradeInformation;
 
 
     // Start is called before the first frame update
@@ -17,5 +26,13 @@ public class UpgradeManager : Singleton<UpgradeManager>
     void Update()
     {
         
+    }
+
+    public void PurchaseUpgrade(int _upgradeID)
+    {
+        var currentUpgrade = resturantUpgradeInformation.FindIndex(_upgradeID);
+
+        currentUpgrade.active = true;
+    
     }
 }
